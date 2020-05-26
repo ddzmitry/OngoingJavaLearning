545a3d76f79c9bce32897ebc093866cdd8697e47
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2020-05-25 23:04:20 -0400

    Scala

--- RockJVMScalaBeginners/src/lectures/part1basics/Functions.scala
+++ RockJVMScalaBeginners/src/lectures/part1basics/Functions.scala
@@ -17,6 +17,33 @@ object Functions extends  App {
 
 
 
+// VOID functinon
+def aFunctionWithSideefects(aString: String) : Unit = println(aString)
 
+  def aBigFunction(n: Int): Int  ={
+  def asmallFUnciton (a: Int, b:Int) : Int = a + b
+    // run function within a function
+    asmallFUnciton(n,math.pow(n,5).toInt)
+  }
+  println(aBigFunction(10))
 
+  def greating(name:String, Age:Int) : Unit = println(f"Hello my name is $name my age is $Age")
+
+  // challanges
+  def factorial(num: Int): Int ={
+    if(num <= 1) num else num * factorial(num-1)
+  }
+  // 3*2*1 = 6
+  // 4 * 3 *2 *1 = 24
+  println(factorial(3))
+  println(factorial(4))
+
+  // fib(n-1) + fib(n-2)
+  def fib( n: Int) : Int = {
+    n match {
+      case 0 | 1 => n
+      case _ => fib(n -1) + fib(n-2)
+    }
+  }
+  println(fib(10))
 }
